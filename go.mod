3652f1ffd63f3851f834541606e95d6d26a2401e
liuhao <czliuhao@qq.com>
2019-06-24 00:08:42 +0800

    v2 version

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/liuhao189/gomodtest
+module github.com/liuhao189/gomodtest/v2
 
 go 1.12
--- gomodtest.go
+++ gomodtest.go
@@ -1,7 +1,21 @@
 package gomodtest
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hi(name string) string {
-	return fmt.Sprintf("Hi,%s!", name)
+func Hi(name, lang string) (string,error) {
+	swicth lang{
+	case "en":
+		return fmt.Sprintf("Hi,%s!",name),nil;
+	case "pr":
+		return fmt.Sprintf("Oi,%s!",name),nil;
+	case "es":
+		return fmt.Sprintf("!Hola,%s!",name),nil;
+	case "fr":
+		return fmt.Sprintf("Bonjour,%s!",name),nil;
+	default:
+		return "",errors.New("unknown language")
+	}
 }
